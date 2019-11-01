9e8c0425f77fd697a0d6f93019029a3bec23430a
Yigit Haciefendioglu <yigith1@gmail.com>
2019-11-01 21:32:40 +0300

    Layout ve Models

---
+++ BlogFall/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BlogFall.Models
+{
+    [Table("Categories")]
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Kategori Adı")]
+        public string CategoryName { get; set; }
+
+
+        public virtual ICollection<Post> Posts { get; set; }
+    }
+}
