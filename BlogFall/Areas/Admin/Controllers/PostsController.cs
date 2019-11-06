e7066d3b3ba87aa27d189a99efcdad5383d25cd1
Yigit Haciefendioglu <yigith1@gmail.com>
2019-11-06 21:32:42 +0300

    Admin Posts

--- BlogFall/Areas/Admin/Controllers/PostsController.cs
+++ BlogFall/Areas/Admin/Controllers/PostsController.cs
@@ -1,16 +1,72 @@
-﻿using System;
+﻿using BlogFall.Areas.Admin.ViewModels;
+using BlogFall.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
 namespace BlogFall.Areas.Admin.Controllers
 {
-    public class PostsController : Controller
+    public class PostsController : AdminBaseController
     {
         // GET: Admin/Posts
         public ActionResult Index()
         {
+            return View(db.Posts.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var post = db.Posts.Find(id);
+
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+
+            db.Posts.Remove(post);
+            db.SaveChanges();
+
+            return Json(new { success = true });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            ViewBag.CategoryId = new SelectList(db.Categories.ToList(), "Id", "CategoryName");
+
+            PostEditViewModel vm = db.Posts.Select(x => new PostEditViewModel
+            {
+                Id = x.Id,
+                CategoryId = x.CategoryId,
+                Content = x.Content,
+                Title = x.Title
+            }).FirstOrDefault(x => x.Id == id);
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(PostEditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Post post = db.Posts.Find(model.Id);
+
+                post.Content = model.Content;
+                post.CategoryId = model.CategoryId;
+                post.Title = model.Title;
+
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryId = new SelectList(db.Categories.ToList(), "Id", "CategoryName");
             return View();
         }
     }
---
+++ BlogFall/Areas/Admin/ViewModels/PostEditViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BlogFall.Areas.Admin.ViewModels
+{
+    public class PostEditViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Kategori")]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        [Display(Name = "Başlık")]
+        public string Title { get; set; }
+
+        [Display(Name = "İçerik")]
+        public string Content { get; set; }
+    }
+}
